8544fa12ddf3b028a104324ccf00bbf3e92e2e27
namsoleo <malihayoub62@gmail.com>
2023-09-07 10:47:25 -0700

    cisfun

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 
 /**
  * main - write to stdout
- * Return: 1
+ * Return: always 0
  */                                                                                                                                                     int main(void)  
 { 
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" ,59);
