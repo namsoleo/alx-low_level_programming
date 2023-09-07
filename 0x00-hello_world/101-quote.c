f5e691d023b905f79188c9d42e137846f8885e81
namsoleo <malihayoub62@gmail.com>
2023-09-07 10:45:58 -0700

    cisfun

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,9 @@
 #include <unistd.h>  
-                                                                                                                                                     int main(void)  
+
+/**
+ * main - write to stdout
+ * Return: 1
+ */                                                                                                                                                     int main(void)  
 { 
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" ,59);
 		return(1);
